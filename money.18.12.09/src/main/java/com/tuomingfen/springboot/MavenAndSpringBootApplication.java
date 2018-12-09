4b15693fa937852bbfec4341899519131f547ad3
user654name <ztqztq@qq.com>
2018-12-09 18:31:21 +0800

    Initial commit

---
+++ money.18.12.09/src/main/java/com/tuomingfen/springboot/MavenAndSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.tuomingfen.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenAndSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenAndSpringBootApplication.class, args);
+	}
+}
