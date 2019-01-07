e7f7c09361bcb290be86988c23b2fccd5f9c10b1
user654name <ztqztq@qq.com>
2019-01-07 17:54:25 +0800

    1 加入lombok 2 别嚷项目烂尾

---
+++ money.18.12.09/src/main/java/com/tuomingfen/springboot/model/Account.java
@@ -0,0 +1,32 @@
+package com.tuomingfen.springboot.model;
+
+import lombok.Data;
+
+/**
+ * @author Tianqi.Zhang
+ * @date 2019/1/7
+ * @time 17:41
+ * @package com.tuomingfen.springboot.model
+ * @project springboot_2018_12
+ * @description
+ */
+@Data
+public class Account {
+
+    /**
+     * 用户id
+     * 记录id
+     * 记录新建日期
+     * 记录修改时期
+     *
+     *
+     * 记录涉及的内容id
+     */
+    String userId;
+    String userId2;
+    String userId32;
+    String userId23;
+    String userI23d;
+
+
+}
---
+++ money.18.12.09/src/main/java/com/tuomingfen/springboot/model/Record.java
@@ -0,0 +1,12 @@
+package com.tuomingfen.springboot.model;
+
+/**
+ * @author Tianqi.Zhang
+ * @date 2019/1/7
+ * @time 17:53
+ * @package com.tuomingfen.springboot.model
+ * @project springboot_2018_12
+ * @description
+ */
+public class Record {
+}
